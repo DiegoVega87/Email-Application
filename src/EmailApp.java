f809fee43e8afcbbbb6f524ef8b4af20f2c0cf8c
DiegoVega87 <diegovega@me.com>
2024-01-25 12:52:46 +0100

    add files to ignore

---
+++ src/EmailApp.java
@@ -0,0 +1,23 @@
+package src;
+
+
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+        Email email = new Email("John", "Smith");
+        System.out.println(email.showInfo());
+
+        System.out.println("Alternate email:"+email.getAlternateEmail()+"\n");
+        email.setAlternateEmail("");
+        email.setAlternateEmail("jhonsmith@me.com");
+        System.out.println("Alternate src.src.Email: " + email.getAlternateEmail() +"\n");
+        email.setPassword("asfgassdg");
+        email.setPassword(("A123bbA_"));
+        email.setDefaultMailboxCapacity(100);
+        email.setDefaultMailboxCapacity(600);
+        System.out.println(email.getMailboxCapacity());
+        System.out.println(email.getEmail());
+    }
+
+}
